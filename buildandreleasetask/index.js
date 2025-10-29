6f96f67bcf9a8d320fa5903226b0d6013f2ca650
Christoph Rogalla <christoph.rogalla@rhenus.com>
2025-10-29 12:07:42 +0100

    feat: add build for update

--- buildandreleasetask/index.js
+++ buildandreleasetask/index.js
@@ -35,12 +35,10 @@ const fs_1 = __importDefault(require("fs"));
 const path = __importStar(require("path"));
 async function run() {
     try {
-        const templatePath = path.join(__dirname, 'template.hbs');
-        console.log('Looking for template at:', templatePath);
-        console.log('Exists?', fs_1.default.existsSync(templatePath));
         const filePath = tl.getInput('filePath', true);
         const outputFilePath = tl.getInput('outputFilePath', true);
         const includeReadActions = tl.getInput('includeReadActions', false) == 'true';
+        const templatePath = path.join(__dirname, 'template.hbs');
         const template = fs_1.default.readFileSync(templatePath, "utf8");
         let fileReader = new JsonFileReader_1.default();
         let changeDetector = new TerraformChangeDetector_1.default();
